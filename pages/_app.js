9178d922069607cc7a2cee056ef839e02d8d34da
ibrahem hassan <hibrahem266@gmail.com>
2023-03-19 15:10:24 +0200

    edit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+const clientSideEmotionCache = createCache({ key: 'css' });
+export default function App({ Component, pageProps , emotionCache = clientSideEmotionCache, }) {
+  return( 
+    <CacheProvider value={emotionCache}>
+  <Component {...pageProps} />
+  </CacheProvider>
+  )
 }
